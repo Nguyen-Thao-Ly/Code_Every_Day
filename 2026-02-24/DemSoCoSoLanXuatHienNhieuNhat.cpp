7634a5fd9ba7cacbc9a21db8ffdb3b44f92547a5
Rikim <rikimngudong@gmail.com>
2026-02-24 22:46:18 +0700

    Add files via upload

---
+++ 2026-02-24/DemSoCoSoLanXuatHienNhieuNhat.cpp
@@ -0,0 +1,38 @@
+//https://codeforces.com/group/NLfcjc5iua/contest/433147/problem/E
+
+#include <bits/stdc++.h>
+using namespace std;
+const long MAXX = 1e6;
+long long a[MAXX +5];
+
+int main(){
+
+    long long n;
+    cin >> n;
+
+    long long cntMAX = 0, MAX = 0;
+    for (long long i = 0; i < n; ++i)
+    {
+        long long k;
+        cin >> k;
+        ++a[k];
+
+        if (a[k] > cntMAX)
+        {
+            cntMAX = a[k];
+            MAX = 1;
+        }
+
+        else if (a[k] == cntMAX)
+        {
+            cntMAX = a[k];
+            ++MAX;
+            //cout << MAX << " " << a[k] << " " << k << endl;
+        }
+
+    }
+
+    cout << cntMAX << " " << MAX;
+
+    return 0;
+}
