e9c94dfaaa9aea0f9e3e16846f74d198f72009e0
Rikim <rikimngudong@gmail.com>
2026-02-26 11:58:11 +0700

    Update B.cpp

--- 2026-02-25/Educational Codeforces Round 187 (Rated for Div. 2)/B.cpp
+++ 2026-02-25/Educational Codeforces Round 187 (Rated for Div. 2)/B.cpp
@@ -1,17 +1,4 @@
-/*
-
-F(x) là tổng các chữ số của x.
-X sẽ là số đẹp nếu Tổng các số của Tổng các số của X = tổng các số của x
-nghĩa là sau khi tính tổng 2 lần thì nó giống với lần đầu.
-
-tổng của 37 là 10, tổng lần 2 là 1
-nếu đổi thành 33 : tổng lần 1 là 6, tổng lần 2 là 6 => thành công
-645 có tổng lần 1 là 15, tổng lần 2 là 6. Sau khi đổi thành 140 thì tổng lần 1 là 5, tổng lần 2 cũng là 5.
-
-Vậy suy ra cách giải quyết là biến tất cả tụi nó thành số có tổng là 1 chữ số và in ra số lần phải biến đổi
-
-*/
-
+// https://codeforces.com/contest/2203/problem/B
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -19,11 +6,8 @@ using namespace std;
 void sol(string st){
 
     long long Bang[10] = {0,0,0,0,0,0,0,0,0,0};
-
-    //cout << st << endl;
-
     long long d = st.size();
-
+    
     long long Sum = 0;
     for (long long i = 0; i < d; ++i)
     {
@@ -31,37 +15,23 @@ void sol(string st){
         Sum += (st[i] - '0');
     }
 
-    //for (long long i = 0; i < 16; ++i) {cout << Bang[i] << " " ;}
-    //cout << " " << Sum << endl;
-
     long long cnt = 0;
-    long long SLL = 0;
-
     long long Ngoai_Le = st[0] - '0';
-    //cout << Ngoai_Le << endl;
 
     long long i = 9;
     while (Sum > 9 && i)
     {
-        //cout << "Ktra " << i <<  " " << Bang[i] << endl;
-
-        //if (Bang[i] == 1) {cout << "Bang 1 ne" << endl;}
-        //if (i == Ngoai_Le) {cout << "Vi Pham ne " << endl;}
         if (Bang[i] == 1 && i == Ngoai_Le) //xu li so dau tien != 0
         {
-            //cout << "runBang1" << endl;
            Bang[1] = 1;
            --Bang[i];
            Sum -= i;
            ++Sum;
            ++cnt;
-           //cout << "SSum" << Sum << endl;
 
         }
         else if (Bang[i]  != 0)
         {
-            //cout << "runBang2" << endl;
-
            --Bang[i];
            Sum -= i;
            ++cnt;
@@ -70,7 +40,6 @@ void sol(string st){
     }
 
 
-    //cout << "Ket qua = " << cnt;
     cout << cnt << "\n";
 }
 
@@ -91,10 +60,7 @@ int main(){
 
     }
 
-
-
-
-
     return 0;
 }
+
 
